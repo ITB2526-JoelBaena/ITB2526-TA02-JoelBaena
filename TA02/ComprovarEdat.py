9be7c04a358dd1cfb662d31413d2156422c661b9
joel.baena.7e9 <joel.baena.7e9@itb.cat>
2025-09-26 13:52:56 +0200

    Canvis

---
+++ TA02/ComprovarEdat.py
@@ -0,0 +1,10 @@
+"""
+Joel Baena Martínez
+13/11/2006
+ASIXc1C
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+edat=int(input("Quina edat tens?"))
+if edat>=18:print("Ets major d'edat")
+print("Programa Finalitzat")
