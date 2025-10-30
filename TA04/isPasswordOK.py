02dafa53c9c9b1a00d143f4a3f8412fa224606d3
joel.baena.7e9 <joel.baena.7e9@itb.cat>
2025-10-30 09:56:52 +0100

    Canvis

---
+++ TA04/isPasswordOK.py
@@ -0,0 +1,8 @@
+PWD = "asdasd"
+intento = int(0)
+clave = input("Dime la clave")
+while clave != PWD: and
+    print("Clave incorrecta")
+    clave = input("Dime la clave")
+print("Bienvenido")
+print("Programa terminado")
